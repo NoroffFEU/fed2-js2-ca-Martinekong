1c83392028cfa8af663df173a31c7ff494f8512b
Martinekong <martinekongsrud@outlook.com>
2025-03-20 17:52:32 +0100

    created template for API class

--- src/js/api/noroffAPI.js
+++ src/js/api/noroffAPI.js
@@ -1,1 +1,95 @@
-// Noroff class with all the endpoint functions
+import { BASE_URL } from "./constants.js";
+
+export default class NoroffAPI {
+  constructor(apiBase = `${BASE_URL}`) {
+    this.apiBase = apiBase
+  }
+
+  auth = {
+    login: async ({email, password}) => {
+      try {
+        //
+      } catch {
+        //
+      }
+    },
+
+    register: async ({name, email, password}) => {
+      try {
+        //
+      } catch {
+        //
+      }
+    },
+
+    logout: () => {
+      try {
+        //
+      } catch {
+        //
+      }
+    }
+  }
+
+  allPosts = {
+    view: async () => {
+      try {
+        //
+      } catch {
+        //
+      }
+    }
+  }
+
+  post = {
+    view: async (id) => {
+      try {
+        //
+      } catch {
+        //
+      }
+    },
+
+    create: async (title) => {
+      try {
+        //
+      } catch {
+        //
+      }
+    },
+
+    delete: async (id) => {
+      try {
+        //
+      } catch {
+        //
+      }
+    },
+
+    update: async (title, id) => {
+      try {
+        //
+      } catch {
+        //
+      }
+    }
+  }
+
+  profile = {
+    view: async () => {
+      try {
+        //
+      } catch {
+        //
+      }
+    },
+
+    update: async () => {
+      try {
+        //
+      } catch {
+        //
+      }
+    }
+  }
+}
