c1f5f00be30398b01cb6c7ef20e474635c19042e
Martinekong <martinekongsrud@outlook.com>
2025-03-21 14:14:44 +0100

    api utils functions

--- src/js/api/noroffAPI.js
+++ src/js/api/noroffAPI.js
@@ -6,24 +6,39 @@ export default class NoroffAPI {
     this.apiBase = apiBase
   }
 
+  utils = {
+    setupHeaders: ({ auth = true, apiKey = true, json = true} = {}) => {
+      const headers = {};
+      if (json) headers["Content-Type"] = "application/json";
+      if (auth) headers["Authorization"] = `Bearer ${getToken()}`;
+      if (apiKey) headers["X-Noroff-API-Key"] = `${API_KEY}`;
+      return headers;
+    },
+
+    handleResponse: async (response) => {
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData?.errors?.[0]?.message || "Unknown error occurred");
+      }
+      return response.json()
+    }
+  }
+
   auth = {
     login: async ({email, password}) => {
       try {
         const response = await fetch(`${this.apiBase}/auth/login`, {
-          headers: { "Content-Type": "application/json" },
+          headers: this.utils.setupHeaders({ auth: false, apiKey: false }),
           method: "POST",
           body: JSON.stringify({email, password})
         })
 
-        if (response.ok) {
-          const { data } = await response.json()
-          saveToken(data.accessToken)
-          saveUsername(data.name)
-          console.log(data)
-          return data;
-        }
-
-        throw new Error(`Could not log in user`)
+        const { data } = await this.utils.handleResponse(response);
+        saveToken(data.accessToken)
+        saveUsername(data.name)
+        console.log(data)
+        console.log(`User successfully logged in`)
+        return data;
         
       } catch(error) {
         console.log(error)
@@ -33,18 +48,14 @@ export default class NoroffAPI {
     register: async ({name, email, password}) => {
       try {
         const response = await fetch(`${this.apiBase}/auth/register`, {
-          headers: { "Content-Type": "application/json" },
+          headers: this.utils.setupHeaders({ auth: false, apiKey: false}),
           method: "POST",
           body: JSON.stringify({name, email, password})
         })
 
-        if (response.ok) {
-          const { data } = await response.json()
-          console.log(`Response: ${response}`)
-          console.log(`Data: ${data}`)
-        }
-
-        throw new Error(`Could not register user`)
+        const { data } = await this.utils.handleResponse(response)
+        console.log(`User registered successfully`)
+        return data;
 
       } catch(error) {
         console.log(error)
@@ -54,6 +65,7 @@ export default class NoroffAPI {
     logout: () => {
       localStorage.removeItem("token");
       localStorage.removeItem("username");
+      console.log(`User successfully logged out`)
       // Redirect to index.html (login page) - need if statements depending on the path
     }
   }
@@ -62,19 +74,13 @@ export default class NoroffAPI {
     view: async () => {
       try {
         const response = await fetch(`${this.apiBase}/social/posts`, {
-          headers: {
-            "Authorization": `Bearer ${getToken()}`,
-            "X-Noroff-API-Key": `${API_KEY}`
-          }
+          headers: this.utils.setupHeaders({ json: false })
         })
 
-        if (response.ok) {
-          const { data } = await response.json()
-          console.log(data)
-          return data;
-        }
-
-        throw new Error(`Failed to get all posts`)
+        const { data } = await this.utils.handleResponse(response)
+        console.log(data);
+        console.log(`Successfully fetched all posts`)
+        return data;
 
       } catch(error) {
         console.log(error)
@@ -86,19 +92,13 @@ export default class NoroffAPI {
     view: async (id) => {
       try {
         const response = await fetch(`${this.apiBase}/social/posts/${id}`, {
-          headers: {
-            "Authorization": `Bearer ${getToken()}`,
-            "X-Noroff-API-Key": `${API_KEY}`
-          }
+          headers: this.utils.setupHeaders({ json: false })
         })
 
-        if (response.ok) {
-          const { data } = await response.json()
-          console.log(data)
-          return data;
-        }
-
-        throw new Error(`Failed to get the post with id ${id}`)
+        const { data } = await this.utils.handleResponse(response)
+        console.log(data)
+        console.log(`Successfully got a single post by its id`)
+        return data;
 
       } catch(error) {
         console.log(error)
@@ -109,22 +109,15 @@ export default class NoroffAPI {
       const newPost = { title: title }
       try {
         const response = await fetch(`${this.apiBase}/social/posts`, {
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${getToken()}`,
-            "X-Noroff-API-Key": `${API_KEY}`
-          },
+          headers: this.utils.setupHeaders(),
           method: "POST",
           body: JSON.stringify(newPost)
         })
 
-        if (response.ok) {
-          const { data } = await response.json()
-          console.log(data)
-          return data;
-        }
-  
-        throw new Error(`Failed to create post with the title: ${title}`)
+        const { data } = await this.utils.handleResponse(response);
+        console.log(data)
+        console.log(`Successfully created a post with the title: ${title}`)
+        return data;
 
       } catch(error) {
         console.log(error)
@@ -134,15 +127,11 @@ export default class NoroffAPI {
     delete: async (id) => {
       try {
         const response = await fetch(`${this.apiBase}/social/posts/${id}`, {
-          headers: {
-            "Authorization": `Bearer ${getToken()}`,
-            "X-Noroff-API-Key": `${API_KEY}`
-          },
+          headers: this.utils.setupHeaders({ json: false }),
           method: "DELETE"
         })
 
         if (response.ok) {
-          console.log(response)
           console.log(`Post deleted successfully`)
           return;
         }
@@ -158,22 +147,15 @@ export default class NoroffAPI {
       const updatedPost = { title: title }
       try {
         const response = await fetch(`${this.apiBase}/social/posts/${id}`, {
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${getToken()}`,
-            "X-Noroff-API-Key": `${API_KEY}`
-          },
+          headers: this.utils.setupHeaders(),
           method: "PUT", 
           body: JSON.stringify(updatedPost)
         })
 
-        if (response.ok) {
-          const { data } = await response.json();
-          console.log(data)
-          return data;
-        }
-  
-        throw new Error(`Failed to update post`)
+        const { data } = await this.utils.handleResponse(response);
+        console.log(data)
+        console.log(`Post updated successfully`)
+        return data;
 
       } catch(error) {
         console.log(error)
@@ -186,19 +168,14 @@ export default class NoroffAPI {
       const name = getUsername()
       try {
         const response = await fetch(`${this.apiBase}/social/profiles/${name}`, {
-          headers: {
-            "Authorization": `Bearer ${getToken()}`,
-            "X-Noroff-API-Key": `${API_KEY}`
-          }
+          headers: this.utils.setupHeaders({ json: false })
         })
 
-        if (response.ok) {
-          const { data } = await response.json()
-          console.log(data)
-          return data;
-        }
+        const { data } = await this.utils.handleResponse(response)
+        console.log(data)
+        console.log(`Successfully viewed the users profile`)
+        return data;
 
-        throw new Error(`Failed to get the profile of ${name}`)
       } catch(error) {
         console.log(error)
       }
@@ -208,26 +185,19 @@ export default class NoroffAPI {
       const name = getUsername()
       try {
         const response = await fetch(`${this.apiBase}/social/profiles/${name}`, {
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${getToken()}`,
-            "X-Noroff-API-Key": `${API_KEY}`
-          },
+          headers: this.utils.setupHeaders(),
           method: "PUT",
           body: JSON.stringify(updates)
         })
 
-        if (response.ok) {
-          const { data } = await response.json()
-          console.log(data)
-          return data;
-        }
+        const { data } = await this.utils.handleResponse(response)
+        console.log(data)
+        console.log(`Successfully updated the users profile`)
+        return data;
 
-        throw new Error(`Failed to update the profile of ${name}`)
       } catch(error) {
         console.log(error)
       }
-    
     }
   }
 }
@@ -243,19 +213,18 @@ const api = new NoroffAPI()
 //api.auth.register({name, email, password})
 //api.auth.logout()
 
-//api.allPosts.view()
+api.allPosts.view()
 
 //api.post.view("8072")
 //api.post.create("hello world!") //created successfully, id: 8083
 //api.post.delete("8083") // deleted successfully, id: 8083
-//api.post.create("hello world!") //created successfully, id:8085
 //api.post.update("updated hello world!", "8085") //updated successfully, id: 8085
 
-// const profile = await api.profile.view()
-// console.log(profile.name)
+//const profile = await api.profile.view()
+//console.log(profile.name)
 // console.log(profile._count.posts)
 // console.log(profile._count.followers)
 
-//api.profile.update({
-//  bio: "Student"
-//})
+// api.profile.update({
+//  bio: "Student at Noroff"
+// })
