4355e24d6f283684c729c9fbab6713efa181a63f
Martinekong <martinekongsrud@outlook.com>
2025-04-02 16:12:01 +0200

    delete post function

---
+++ src/js/ui/post/delete.js
@@ -0,0 +1,16 @@
+import NoroffAPI from "../../api/noroffAPI.js";
+
+const api = new NoroffAPI()
+
+const params = new URLSearchParams(window.location.search);
+const postId = params.get("id");
+
+async function deletePost() {
+  const confirmDelete = confirm("Are you sure you want to delete this post?");
+  
+  if (!confirmDelete) return;
+
+  await api.post.delete(postId);
+}
+
+document.getElementById("delete-post-btn").addEventListener("click", deletePost);
