8848b3e55051b01b1ae9d9160ac9bbfdc70ce649
Martinekong <martinekongsrud@outlook.com>
2025-04-09 15:42:21 +0200

    Updates

--- assets/authGuard-DDOfWTFa.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./app-DhhahDUS.js";function a(){o()||(alert("You must be logged in to view this page"),window.location.href="/auth/login.html")}export{a};
---
+++ assets/authGuard-dow9MYyi.js
@@ -0,0 +1,1 @@
+import{a as o}from"./app-XOvmieRm.js";function a(){o()||(alert("You must be logged in to view this page"),window.location.href="./auth/login.html")}export{a};
